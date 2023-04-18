69ae291b5e03b19f502f8c2967696d35e51d3e11
Sharleen Beniamin <sharleenb16@gmail.com>
2023-04-18 16:06:07 +0000

    refactor getAllReservations to use query code

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -65,7 +65,6 @@ const addUser = function (user) {
   return pool.query(`INSERT INTO users (
     name, email, password) VALUES ($1, $2, $3) RETURNING *` , [userArray.name, userArray.email, userArray.password])
     .then((result) => {
-      console.log('worked!');
       console.log(result.rows);
     })
     .catch((err) => {
@@ -85,7 +84,20 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`SELECT reservations.*, properties.*, AVG(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id=$1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`, [guest_id, limit])
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 /// Properties
